b2b3e19361acde3155e54ff35cc831e31b837d0b
DESKTOP-T0IK3GO\sluer <kyongahjulieseo@gmail.com>
2021-04-02 15:52:33 +0900

    add passport to use cookies and sessions

--- app.js
+++ app.js
@@ -5,82 +5,107 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const port = 3000;
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//1. require 3 packages
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-console.log(process.env.API_KEY);
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//2. set up session
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+//3. initialize passport and use passport to manage session
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = new mongoose.Schema ({
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+//4. use schema as a plugin to use passportLocalMongoose
+userSchema.plugin(passportLocalMongoose);
 
-// 패스워드만 암호화한다는것을 명시해줘여한다.
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+const User = new mongoose.model("User", userSchema);
 
-const User = new mongoose.model("User",userSchema);
+//5. use passportLocalMongoose to create a local log in strategy
+passport.use(User.createStrategy());
 
-app.get("/", function(req,res){
+//6. set passport to serialise and deserialise our user
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
-  const newUser = new User({
-  email: req.body.username,
-  password: req.body.password
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
 });
 
-newUser.save(function(err){
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req, res) {
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
     if(err){
       console.log(err);
-    } else{
-      res.render("secrets");
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
 });
 
-app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
+app.post("/login", function(req, res) {
 
-  //데이터베이스상의 email과 login페이지에서 사용자가 입력한 email(=username)이 일치하는지 확인
-  User.findOne({email: username}, function(err, foundUser){
-    if(err){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if (err){
       console.log(err);
-    } else{
-      //일치한다면 데이터베이스상의 password와 사용자가 입력한 password가 일치하는지 확인
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
-      }
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
 });
-
-
-
-
-
-
-
-
 
 
 app.listen(port, () => {
